f51b3b2f3f2b910015a5e1c1c178fe64c0afc3c1
unknown <ba7399@gmail.com>
2019-01-28 17:11:05 -0500

    first commit

---
+++ lab21/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lab21.Models;
+
+namespace lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterClass NewPerson)
+        {
+
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = $"Thanks for signing up {NewPerson.FirstName} {NewPerson.LastName}!!!";
+                return View("Success");
+            }
+            else
+            {
+                ViewBag.Error = "Oops! Try Again!";
+                return View();
+            }
+
+           
+        }
+
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
+
+        
---
+++ lab21/Models/RegisterClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace lab21.Models
+{
+    public class RegisterClass
+    {
+        //I dont think this level of validation will work with the html form, tho I could (probably) be wrong.
+        [Required(ErrorMessage = "Enter Name PLease")]
+        [MaxLength(20)]
+        [RegularExpression(@"^[A-Za-z.\s_-]+$", ErrorMessage = "Invalid Character Input")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Enter Name PLease")]
+        [MaxLength(20)]
+        [RegularExpression(@"^[A-Za-z.\s_-]+$", ErrorMessage = "Invalid Character Input")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Email Required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Date of Birth Required")]
+        public DateTime DoB { get; set; }
+        [Required(ErrorMessage = "Phone Number Required")]
+        public int PhoneNumber { get; set; }
+    }
+}
